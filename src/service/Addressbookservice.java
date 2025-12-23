8e2acc69c0780c6ff8e42563bd8c86926ba0a722
karrichandu2003 <chandukarri790@gmail.com>
2025-12-23 14:45:48 +0530

    UC2: Add contact feature

--- src/service/Addressbookservice.java
+++ src/service/Addressbookservice.java
@@ -1,4 +1,15 @@
 package service;
 
+import model.Contact;
+import java.util.ArrayList;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+
 public class Addressbookservice {
+    ArrayList<Contact> contacts=new ArrayList<Contact>();
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
 }
